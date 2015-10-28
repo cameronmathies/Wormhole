523c37d45f46d504e750eefd59040c77d1e7f964
cameronmathies <cmathies@IT005.mr.preble.com>
2015-10-28 13:19:40 -0400

    Port

---
+++ Wormhole Unity Project/Assets/Code/Telaport.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Telaport : MonoBehaviour {
+    public GameObject Port; // Get's the Game Object that has the player tag on it.
+    public Transform SpawnPoints; // Get's the transform of the spawn points.
+	// Use this for initialization
+	
+    void OnTriggerEnter(Collider col)
+    {
+    if(col.tag == "Port") // If Game Object has the tag "Player" then we teleport it.
+        {
+            Port = col.transform.gameObject;
+        }
+    }
+}
