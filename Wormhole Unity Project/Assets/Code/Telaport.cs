4c4e3b040682f4c543b50ef75f0005d67e5b63d4
cameronmathies <cmathies@IT005.mr.preble.com>
2015-10-29 13:33:00 -0400

    NO Error

--- Wormhole Unity Project/Assets/Code/Telaport.cs
+++ Wormhole Unity Project/Assets/Code/Telaport.cs
@@ -1,23 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Telaport : MonoBehaviour {
+//public class Telaport : MonoBehaviour {
     /// <summary>
     /// 
     /// </summary>
-    public GameObject Player; // Get's the Game Object that has the player tag on it.
-    public Transform SpawnPoints; // Get's the transform of the spawn points.
+  //  public GameObject Player; // Get's the Game Object that has the player tag on it.
+  //  public Transform SpawnPoints; // Get's the transform of the spawn points.
                                   // Use this for initialization
 
     //  void OnTriggerEnter (Collider Player)
     //void OnTriggerEnter()
-    {
-    if(col.tag == "Player") // If Game Object has the tag "Player" then we teleport it.
+  //  {
+  //  if(col.tag == "Player") // If Game Object has the tag "Player" then we teleport it.
      //   {
        //     Player = col.transform.parent.gameObject.transform.parent.gameObject;
-            Player.transform.position = SpawnPoints.transform.position;
-            Player.transform.rotation = SpawnPoints.transform.rotation;
-        Debug.Log("warking?");
+     //       Player.transform.position = SpawnPoints.transform.position;
+       //     Player.transform.rotation = SpawnPoints.transform.rotation;
+    //    Debug.Log("warking?");
       //  }
-    }
-}
+  //  }
+//}
